a3258652a2211ae6a015a0b46b771a4ddb3cd1c2
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-04-30 13:10:01 +0300

    Update transaction.py

--- chain/transaction.py
+++ chain/transaction.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class Transaction:
     sender: str
     recipient: str
     amount: float
+
+    def to_dict(self):
+        return {
+            'sender': self.sender,
+            'recipient': self.recipient,
+            'amount': self.amount
+        }
